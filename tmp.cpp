f8b17ed2605704c93d91e3791843a9ce7a310900
Sunstreamy <sunstreamy@mail.tust.edu.cn>
2025-07-14 21:18:10 +0800

    tmp

--- tmp.cpp
+++ tmp.cpp
@@ -1,21 +1,24 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long i64;
 typedef __int128_t i128;
-typedef pair<int, int> PII;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef bitset<20> b20;
 const int M = 1e9 + 7;
 #define N 200005
+#define int long long
 
 void solve()
 {
 }
 
-int main()
+signed main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+    solve();
 
     return 0;
 }
