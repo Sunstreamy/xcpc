b45723758e51b63c4468d333b0c6c7c8c5c15c39
Sunstreamy <sunstreamy@mail.tust.edu.cn>
2025-07-15 01:33:30 +0000

    2025年7月15日 09:33

--- data_structure/ftr.cpp
+++ data_structure/ftr.cpp
@@ -1,61 +1,51 @@
+//
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
+typedef __int128_t i128;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+const int M = 1e9 + 7;
+#define N 20000
+#define int long long
 
 void solve()
 {
     int n;
     cin >> n;
-    vector<int> a(n + 1), tr(n + 1);
-
-    for (int i = 1; i <= n; i++)
+    // 先读入所有数据
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
     {
-        int x;
-        cin >> x;
-        a[x] = i;
+        cin >> a[i];
     }
 
-    int num = n * (n - 1) / 2;
-
-    for (int i = 1; i <= n; i++)
+    vector<int> b(a);
+    sort(b.begin(), b.end());
+    b.erase(unique(b.begin(), b.end()), b.end());
+    int m = b.size();
+    for (int i = 0; i < n; i++)
     {
-        int x;
-        cin >> x;
-        x = a[x];
+        a[i] = lower_bound(b.begin(), b.end(), a[i]) - b.begin() + 1;
+    }
 
+    VI tr(m + 1, 0);
+    int ans = n * (n - 1) / 2;
+    for (int i = 0; i < n; i++)
+    {
+        int x = a[i];
         for (int j = x; j; j -= (j & -j))
-            num -= tr[j];
-
-        for (int j = x; j <= n; j += (j & -j))
+            ans -= tr[j];
+        for (int j = x; j <= m; j += (j & -j))
             tr[j]++;
     }
-
-    if (num & 1)
-        cout << "A";
-    else
-        cout << "B";
-
-    for (int i = 1; i < n; i++)
-    {
-        char opt;
-        int l, r, d;
-        cin >> opt >> l >> r >> d;
-        num += (l - r + 1 - d) * d;
-
-        if (num & 1)
-            cout << "A";
-        else
-            cout << "B";
-    }
-    cout << "\n";
+    cout << ans << '\n';
 }
-
 signed main()
 {
-    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int T = 1;
-    cin >> T;
-    for (int i = 1; i <= T; i++)
-        solve();
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+
     return 0;
 }
