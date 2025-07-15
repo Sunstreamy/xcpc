fe06b6d735d723099a8986f6f1b2482d0500eebf
Sunstreamy <sunstreamy@mail.tust.edu.cn>
2025-07-15 01:15:23 +0000

    2025年7月15日 09:15

--- nk1/l.cpp
+++ nk1/l.cpp
@@ -12,7 +12,9 @@ const int M = 1e9 + 7;
 struct BIT
 {
     int n;
-    vector<int> tree;
+    //初始化一个可以容纳 n 个数据（外加一个额外索引）的树状数组
+        vector<int>
+            tree;
     BIT(int n) : n(n), tree(n + 1, 0) {}
     // 单点更新：在位置 idx 加上 delta
     void update(int idx, int delta)
