e222cde99684ea74ff137f8d185b65a0df5e7d56
Sunstreamy_mac <2927784670@qq.com>
2025-07-21 11:24:08 +0000

    2025年7月21日 19:24

--- xcpc-mytemplate/算法/数据结构/线段树.cpp
+++ xcpc-mytemplate/算法/数据结构/线段树.cpp
@@ -1,6 +1,7 @@
-﻿/**   线段树（SegmentTree+Info+Merge 初始赋值+单点修改+区间合并）
- *    2022-04-23: https://codeforces.com/contest/1672/submission/154766851
+﻿/** 
+    线段树（SegmentTree+Info+Merge 初始赋值+单点修改+区间合并） 
 **/
+#include <bits/stdc++.h>
 template<class Info,
     class Merge = std::plus<Info> >
 struct SegmentTree {
