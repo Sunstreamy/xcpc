d12f291c4b744dbb14297a8e7aefa8b8fd644825
Sunstreamy_mac <2927784670@qq.com>
2025-08-01 20:57:05 +0800

    upd template folder

---
+++ template/tt.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve() {
+    int 
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int _;
+    cin >> _;
+    while (_--) solve();
+    return 0;
+}
