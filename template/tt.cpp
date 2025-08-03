58293ad1fb2cb1aafb3e4bbedfbeea7cb9fa94c8
Sunstreamy_mac <2927784670@qq.com>
2025-08-03 12:58:32 +0800

    tt

--- template/tt.cpp
+++ template/tt.cpp
@@ -3,16 +3,17 @@ using namespace std;
 #define int long long
 constexpr int mod = 998244353;
 
-void solve() {
-    
+void solve()
+{
 }
 
-signed main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int _;
     cin >> _;
-    while (_--) solve();
+    while (_--)
+        solve();
     return 0;
 }
