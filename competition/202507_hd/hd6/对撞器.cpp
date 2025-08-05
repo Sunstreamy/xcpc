337aad3e55772ece8ea09c4307bb69442eed1d30
Sunstreamy <sunstreamy@mail.tust.edu.cn>
2025-08-05 10:44:12 +0800

    competition

---
+++ competition/202507_hd/hd6/对撞器.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+using u128 = unsigned __int128;
+using i128 = __int128;
+using b20 = bitset<20>;
+
+constexpr int M = 1e9 + 7;
+constexpr i64 linf = 0x3f3f3f3f3f3f3f3fLL;
+constexpr int maxm = 400005;
+constexpr int maxn = 200005;
+//------------------------------------------------------------------
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    i64 mx = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        if (a[i] > mx) mx = a[i];
+    }
+    if (n <= 2) {
+        cout << 0 << '\n';
+        return;
+    }
+    i64 ans = 0;
+    ans +=mx * (n - 3) + max(a[0], a[n - 1]);
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int _;
+    cin >> _;
+    while (_--) solve();
+    return 0;
+}
