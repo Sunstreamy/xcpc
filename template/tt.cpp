15f279d1dcaff1795f17cbcfd7be0217d7d70b74
Sunstreamy <2927784670@qq.com>
2026-04-28 08:43:05 +0800

    upd tt

--- template/tt.cpp
+++ template/tt.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll= long long;
+using ll = long long;
+
 const int MOD[] = {998244353, 1000000007};
+const int Inf = 0x3f3f3f3f;
+const ll Linf = 0x3f3f3f3f3f3f3f3fLL;
 
 void solve() {
-    
+
 }
 
 int main() {
